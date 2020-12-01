d6ee0e2a2daa92969bd0cb940db1eed236a96124
Abhishek Rao <arao8732@gmail.com>
2020-12-01 22:29:17 +0530

    footer updated

--- bytecodelearners-vue-js/src/components/ByteCodeFooter.vue
+++ bytecodelearners-vue-js/src/components/ByteCodeFooter.vue
@@ -8,7 +8,7 @@
         <div class="bcl-line"></div>
         <div class="bcl-footer middle">
             <div class="bcl-follow-us-on">
-            <!-- <span>follow us</span> -->
+            <span>follow us</span>
                 <div class="bcl-social-links">
                     <a href="https://www.instagram.com/bytecode_learners/" target="_blank"><v-icon class="bcl-social-media-icons"  dark>mdi-instagram</v-icon></a>
                     <a href="https://www.instagram.com/bytecode_learners/" target="_blank"><v-icon class="bcl-social-media-icons" href="" dark>mdi-twitter</v-icon></a>
@@ -24,7 +24,6 @@
 
 <script>
 export default {
-    name:"ByteCodeFooter",
 
 }
 </script>
@@ -51,11 +50,51 @@ footer{
     display: flex;
     flex-direction: column;
 }
+.bcl-follow-us-on{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.bcl-follow-us-on span{
+    text-align: center;
+    font-size: 2.5em;
+}
 .bcl-social-links{
     display: flex;
     flex-direction: row;
+    color: black;
+    text-align: center;
+    justify-content: center;
+    margin: 10% 0 0 0;
 }
 .bcl-social-links>a{
-    margin: 2%;
+    margin: 4%;
+    text-decoration: none;
+}
+.bcl-social-media-icons{
+    color: black;
+    font-size: 3em;
+    text-align: center;
+    text-decoration: none;
+}
+.bcl-social-media-icons:hover{
+    color: red;
+    text-decoration: none;
+ 
+}
+@media only screen and (max-width: 600px){
+    .bcl-follow-us-on span{
+        font-size: 2em;
+    }
+    .bcl-social-links{
+        flex-direction: column ;
+    }
+    .bcl-social-media-icons{
+        font-size: 4em;
+    }
+    .bcl-social-links>a{
+        margin: 2%;
+    }
+
 }
 </style>
