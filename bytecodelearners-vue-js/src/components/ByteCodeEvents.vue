6df38ee2d1b89ade5036a405fd034275156bb1c6
bytecode <kumarnocturnal@gmail.com>
2020-12-03 11:28:13 +0530

    removed not required code

--- bytecodelearners-vue-js/src/components/ByteCodeEvents.vue
+++ bytecodelearners-vue-js/src/components/ByteCodeEvents.vue
@@ -7,7 +7,7 @@
        <div class="container ">
         <div class="row ">
          <v-card class="col-sm-12  col-md-6 bcl-small-container-1" outlined   color="rgba(0,0,0,0.1)">
-              <center><h1>Upcoming Event</h1></center>
+              <center><h1>UPCOMING EVENTS</h1></center>
               <div class="bcl-current-event-img">
                   <center><img :src="logo"></center>
               </div>
@@ -18,15 +18,19 @@
          </v-card>
         <v-card class="col-sm-12  center col-md-6 bcl-small-container-2"  outlined color="rgba(0,0,0,0.1)">
          
-           <center><h1>Previous Events</h1></center>
+           <center><h1>PREVIOUS EVENTS</h1></center>
            <div class="bcl-previous-events">
-               <div class="line1">
-               <img :src="logo">
-               <img :src="logo">
+               <div class="bcl-previous-event-content">
+                <img :src="logo">
                </div>
-               <div class="line2">
-               <img :src="logo">
-               <img :src="logo">
+                <div class="bcl-previous-event-content">
+                <img :src="logo">
+               </div>
+                <div class="bcl-previous-event-content">
+                <img :src="logo">
+               </div>
+                <div class="bcl-previous-event-content">
+                <img :src="logo">
                </div>
 
            </div>
@@ -59,59 +63,43 @@ export default {
   font-weight: 700;
 }
 .bcl-events{
-  /* border:1px solid black; */
   min-height: 50vh;
 }
 .bcl-events .events-heading{
       text-align: center;
-      /* border: 1px solid black; */
       font-size: 4em;
       font-weight: 700;
       margin: 0;
-      /* width: 78%; */
 }
 .bcl-events  .typer{
     font-size: 1em;
     margin: 0;
-    /* border: 1px solid black; */
     text-align: center;
     align-content: center;
     display: flex;
     justify-content: center;
 }
-/* .bcl-small-container-1{
-    border: 1px solid red; 
-     margin: 1%; 
-}  */
 .bcl-current-event{
     text-align: center;
 }
-/* .bcl-small-container-2{
-    border: 1px solid red;
-    margin: 1%;
-} */
 
-.bcl-previous-events .line1{
-    text-align: center;
+.bcl-previous-events{
+
     display: flex;
-    margin: 2%;
+    flex-direction: row;
+    flex-wrap: wrap;
     justify-content: center;
+    align-content: center;
 }
-.bcl-previous-events .line1 img{
+.bcl-previous-event-content{
+    width: fit-content;
+    margin: 5px;
+}
+
+.bcl-previous-events img{
     height: 12em;
     margin: 2%;
 }
-.bcl-previous-events .line2{
-    text-align: center;
-    display: flex;
-    margin: 2%;
-    justify-content: center;
-}
-.bcl-previous-events .line2 img{
-    height: 12em;
-    margin: 2%;
-}
-
 
 @media only screen and (max-width:500px)
 {
@@ -121,17 +109,15 @@ export default {
   .bcl-current-event-img img{
       width:12em;
   }
-  .bcl-previous-events .line1{
-      flex-direction: column;
-  }
-  .bcl-previous-events .line1 img{
-      width:12em;
-  }
-  .bcl-previous-events .line2{
-      flex-direction: column;
-  }
-  .bcl-previous-events .line2 img{
-      width:12em;
+  
+.bcl-previous-event-content{
+    width: fit-content;
+    margin: 5px;
+}
+
+  .bcl-previous-events img{
+      width:150px;
+      height: auto;
   }
 
 }
