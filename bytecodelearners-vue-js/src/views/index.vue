0ea9a35b4cab8e3abaabf4310cb9ff8c72f7c999
bytecode <kumarnocturnal@gmail.com>
2020-12-04 13:05:45 +0530

    removed example of ByteCodeFormContainer

--- bytecodelearners-vue-js/src/views/index.vue
+++ bytecodelearners-vue-js/src/views/index.vue
@@ -2,20 +2,7 @@
   <div class="homepage">
     
     <Introduction/>
-    <div id="bcl-index-main-content" class="contents">
-
-      <!-- EXAMPLE OF ByteCodeFormContainer -->
-      <!-- <ByteCodeFormContainer @submit="formSubmit">
-        <template :formData="formData">
-          <input type="text" name="first_name" v-model="formData.first_name">
-          <input type="text" name="middle_name" v-model="formData.middle_name">
-          <input type="text" name="last_name" v-model="formData.last_name">
-          <input type="text" name="email" v-model="formData.email">
-          <input type="text" name="mobile_number" v-model="formData.mobile_number">
-        </template>
-      </ByteCodeFormContainer> -->
-
-    </div>
+    <div id="bcl-index-main-content" class="contents"></div>
     <div id="bcl-index-main-content" class="contents"></div>
     <v-container>
     <About/>
@@ -35,7 +22,6 @@
 </template>
 
 <script>
-import ByteCodeFormContainer from "@/components/ByteCodeFormContainer";
 import ByteCodeSlider from "@/components/ByteCodeSlider"
 import Introduction from "@/components/Introduction"
 import PhotoGallery from "@/components/PhotoGallery"
@@ -57,8 +43,6 @@ export default {
     About,
     ByteCodeSlider,
     PhotoGallery,
-    ByteCodeFormContainer,
-    
   },
   methods:{
     formSubmit(e)
