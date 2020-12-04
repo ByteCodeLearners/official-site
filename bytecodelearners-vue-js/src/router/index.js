e4597afc6eebd312d4befee3b372798120bf15c5
bytecode <kumarnocturnal@gmail.com>
2020-12-04 13:05:14 +0530

    registration route made

--- bytecodelearners-vue-js/src/router/index.js
+++ bytecodelearners-vue-js/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from "@/views/index"
 import NotFoundPage from "@/views/404"
+import register from "@/components/MemberRegistration"
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path:"/register",
+    name:"register",
+    component:register
   },
   {
     path:"*",
