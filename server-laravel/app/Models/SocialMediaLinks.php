e04a8cd6ce5923617363847ab81063a7d0c40fb3
bytecode <kumarnocturnal@gmail.com>
2020-12-08 23:23:02 +0530

    mutators for socialMediaLinks Model

--- server-laravel/app/Models/SocialMediaLinks.php
+++ server-laravel/app/Models/SocialMediaLinks.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SocialMediaLinks extends Model
 {
     use HasFactory;
-    protected $fillable=["facebook","instagram","linkedin","twitter","github","youtube"];
+    protected $fillable=["id","facebook","instagram","linkedin","twitter","github","youtube"];
     protected $table="social_media_links";
     protected $primaryKey="id";
     public $timestamps = false;
@@ -22,5 +22,48 @@ class SocialMediaLinks extends Model
     ];
 
     protected $hidden=["id"];
+
+    public function setFacebookAttribute($value)
+    {
+        if(!is_null($value))
+            return $this->attributes["facebook"]=$value;
+        else
+            return $this->attributes["facebook"]='';
+    }
+    public function setInstagramAttribute($value)
+    {
+        if(!is_null($value))
+            return $this->attributes["instagram"]=$value;
+        else
+            return $this->attributes["instagram"]='';
+    }
+    public function setTwitterAttribute($value)
+    {
+        if(!is_null($value))
+            return $this->attributes["twitter"]=$value;
+        else
+            return $this->attributes["twitter"]='';
+    }
+    public function setLinkedinnAttribute($value)
+    {
+        if(!is_null($value))
+            return $this->attributes["linkedin"]=$value;
+        else
+            return $this->attributes["linkedin"]='';
+    }
+    public function setGithubAttribute($value)
+    {
+        if(!is_null($value))
+            return $this->attributes["github"]=$value;
+        else
+            return $this->attributes["github"]='';
+    }
+    public function setYoutubeInAttribute($value)
+    {
+        if(!is_null($value))
+            return $this->attributes["youtube"]=$value;
+        else
+            return $this->attributes["youtube"]='';
+    }
 
 }
