d949f4f3bca9c498f58da4a085242bef47a3d449
bytecode <kumarnocturnal@gmail.com>
2020-12-08 23:23:23 +0530

    mutators for UserDetails Model

--- server-laravel/app/Models/UserDetails.php
+++ server-laravel/app/Models/UserDetails.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDetails extends Model
 {
+//$2y$10$LJyyE0RK5DwgydfwGgEe.OEkP0AjnNEpM4pdHbw2Ytn4j45iP7k9m
     use HasFactory;
     protected $table="user_details";
-    protected $fillable=["first_name", "middle_name", "last_name", "mobile_number", "email", "image", "users_id","social_media_links_id"];
+    protected $fillable=["id","first_name", "middle_name", "last_name", "mobile_number", "email", "image", "users_id","social_media_links_id"];
     public $timestamps=false;
     protected $primaryKey="id";
     protected $casts = [
@@ -23,12 +24,19 @@ class UserDetails extends Model
         "users_id"=>"int"
 
     ];
-
+    private $nullable=["middle_name"];
     protected $hidden=["id","users_id","social_media_links_id"];
     public function userRole(){
         return $this->hasOne(Roles::class,"id","role_id");
     }
     public function socialMediaLinks(){
         return $this->hasOne(SocialMediaLinks::class,"id","social_media_links_id");
+    }
+    public function setMiddleNameAttribute($value)
+    {
+        if(!is_null($value))
+        return $this->attributes["middle_name"]=$value;
+        else
+            return $this->attributes["middle_name"]='';
     }
 }
