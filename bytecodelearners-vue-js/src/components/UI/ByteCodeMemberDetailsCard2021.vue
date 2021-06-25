bb6ac0a20bdc94859fb7a72e2255e0b615181a57
Abhishek Rao <arao8732@gmail.com>
2021-06-25 13:58:07 +0530

    member page added

--- bytecodelearners-vue-js/src/components/UI/ByteCodeMemberDetailsCard2021.vue
+++ bytecodelearners-vue-js/src/components/UI/ByteCodeMemberDetailsCard2021.vue
@@ -1,15 +1,17 @@
 <template>
  <v-card>
       <div class="bcl-members-details-container">
-        <!-- <div class="bcl-members-image" :title="getFullName">
-                  <img :src="details.user_details.image |prependImageUrl" :alt="getFullName">
-              </div>
-              <div class="bcl-members-details">
-                  <p>{{getFullName}}</p>
-              </div>
+  <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front" :title="getFullName">
+           <img :src="details.user_details.image |prependImageUrl" :alt="getFullName">
+          </div>
+          <div class="flip-card-back">
+           <h2>{{getFullName}}</h2>
+            <!-- <p>Junior</p> -->
+            <!-- <p>{{details.user_details.year}}</p> -->
+            <template v-if="details.user_details.social_media_links">
               <div class="bcl-members-social-icon">
-                  
-                
                 <div class="bcl-social-links">
                     <a class="instagram" v-if="details.user_details.social_media_links.instagram" :href="details.user_details.social_media_links.instagram" target="_blank"><v-icon class="bcl-social-media-icons"  dark>mdi-instagram</v-icon></a>
                     <a class="twitter" v-if="details.user_details.social_media_links.twitter" :href="details.user_details.social_media_links.twitter"  target="_blank"><v-icon class="bcl-social-media-icons" href="" dark>mdi-twitter</v-icon></a>
@@ -17,19 +19,14 @@
                     <a class="github" v-if="details.user_details.social_media_links.github" :href="details.user_details.social_media_links.github" target="_blank"><v-icon class="bcl-social-media-icons" dark>mdi-github</v-icon></a>            
                     <a class="youtube" v-if="details.user_details.social_media_links.youtube" :href="details.user_details.social_media_links.youtube" target="_blank"><v-icon class="bcl-social-media-icons" href="" dark>mdi-youtube</v-icon></a>
                     <a class="linkedin" v-if="details.user_details.social_media_links.linkedin" :href="details.user_details.social_media_links.linkedin" target="_blank"><v-icon class="bcl-social-media-icons" dark>mdi-linkedin</v-icon></a>
-                </div>
-                
-              </div>
-  </div> -->
-  <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" :title="getFullName">
-           <img :src="details.user_details.image |prependImageUrl" :alt="getFullName">
-          </div>
-          <div class="flip-card-back">
-           <p>{{getFullName}}</p>
-            <p>Junior</p>
-            <p>2021</p>
+                </div>    
+            </div>
+            </template>
+            <template v-else>
+              
+            </template>
+            
+
           </div>
         </div>
    </div>
@@ -147,5 +144,95 @@ img{
   background-color: rgba(0, 180, 0, 0.4);
   color: white;
   transform: rotateY(180deg);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+}
+
+
+.bcl-social-links
+{
+  padding-left:0;
+  margin-bottom:4px;
+  margin-top: 15px;
+  list-style:none;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+
+}
+
+.bcl-social-links a{
+  background-color:#eceeef;
+  color:#818a91;
+  font-size:16px;
+  text-decoration: none;
+  display:inline-block;
+  line-height:44px;
+  width:44px;
+  height:44px;
+  text-align:center;
+  margin-right:8px;
+  border-radius:100%;
+  -webkit-transition:all .2s linear;
+  -o-transition:all .2s linear;
+  transition:all .2s linear
+}
+.bcl-social-links a:active,.bcl-social-links a:focus,.bcl-social-links a:hover
+{
+  color:#fff;
+  background-color:#29aafe
+}
+.bcl-social-links.size-sm a
+{
+  line-height:34px;
+  height:34px;
+  width:34px;
+  font-size:14px
+}
+.bcl-social-links a.facebook:hover
+{
+  background-color:#3b5998
+}
+.bcl-social-links a.twitter:hover
+{
+  background-color:#00aced
+}
+.bcl-social-links a.instagram:hover
+{
+  background-color:#007bb6
+}
+.bcl-social-links a.github:hover
+{
+  background-color:#ea4c89
+}
+.bcl-social-links a.linkedin:hover
+{
+  background-color:#00aced
+}
+.bcl-social-links a.youtube:hover
+{
+  background-color: #FF0000
+}
+
+.bcl-social-links
+{
+  text-align:right
+}
+.bcl-social-links a
+{
+  width:40px;
+  height:40px;
+  text-decoration: none;
+  line-height:40px;
+  margin-left:6px;
+  margin-right:0;
+  margin-right:15px;
+  margin-top: 8px;
+  border-radius:100%;
+  background-color:#33353d
 }
 </style>
--- bytecodelearners-vue-js/src/views/members/index.vue
+++ bytecodelearners-vue-js/src/views/members/index.vue
@@ -27,9 +27,15 @@
         class="tabs"
         style="display:flex; flex-wrap:wrap;flex-direction:row;justify-content: space-between;margin:10px;"
       >
-      <div class="bcl-group-slider-content" v-for="(i ,key) in item.members" :key="key">
-            <MembersDetailsCard2021 :details="i"/>
-      </div>
+      <template v-if="item.members!=null">
+        <div class="bcl-group-slider-content" v-for="(i ,key) in item.members" :key="key">
+          <MembersDetailsCard2021 :details="i"/>
+        </div>
+      </template>
+      <template v-else>
+        <InitiatorCards class="bcl-group-slider-content"  v-for="(initiator,key) in initiators" :key="key" :details="initiator"/>
+      </template>
+      
       </v-tab-item>
       
     </v-tabs-items>
@@ -40,36 +46,82 @@
 <script>
 import API from "../../config/api"
 import MembersDetailsCard2021 from "../../components/UI/ByteCodeMemberDetailsCard2021"
-import MembersDetailsCard from "../../components/UI/ByteCodeMemberDetailsCard"
+// import MembersDetailsCard from "../../components/UI/ByteCodeMemberDetailsCard"
 // import ByteCodeSlider2021 from "../../components/Layout/ByteCodeSlider2021";
+import InitiatorCards from "../../components/UI/InitiatorCards";
+
 export default {
     name:'BytecodeMembers',
      data () {
       return {
         tab: null,
         items: [
+          { year: '2018', members: null},
           { year: '2019', members: null},
           { year: '2020', members: null},
           // { year: '2021', members: null},
         ],
+        initiators:[
+                    {
+                        fullName:"Dipu Kumar Sah",
+                        imageUrl:"http://bytecodelearners.club/initiators-image/dipu-kumar-sah.jpg",
+                        socialLinks:{
+                            github:"https://github.com/dipu-kr-sah",
+                            email:"dipuxah7@gmail.com",
+                            linkedin: "https://www.linkedin.com/in/dipu-kumar-sah-07b308181/",
+                        }
+                    },
+                    {
+                        fullName:"Yash Raj",
+                        imageUrl:"http://bytecodelearners.club/initiators-image/yash-raj.jpg",
+                        socialLinks:{
+                            github:"https://github.com/180905",
+                            email:"yash180905@gmail.com",
+                            linkedin: "https://www.linkedin.com/in/yash-raj-3719111b2/",
+                        }
+
+                    },
+                    {
+                        fullName:"Raj Patel",
+                        imageUrl:"http://bytecodelearners.club/initiators-image/raj-patel.jpg",
+                        socialLinks:{
+                            github:"https://github.com/rajpatel2619",
+                            email:"kakhilesh79@gmail.com",
+                            linkedin :"https://www.linkedin.com/in/rajpatel2619/",
+
+                        }
+                    },
+                    {
+                        fullName:"Shantanu Pratap",
+                        imageUrl:"http://bytecodelearners.club/initiators-image/shantanu-pratap.jpg",
+                        socialLinks:{
+                            linkedin: "https://www.linkedin.com/in/shantanu-p-291b991b4",
+                            github:"https://github.com/180896-cse",
+                            email:"shantanupratap180896@gmail.com",
+                            
+                        }
+                    },
+                ]
+        
       }
     },
     components:{
         MembersDetailsCard2021,
-        MembersDetailsCard,
+        // MembersDetailsCard,
+        InitiatorCards,
     },
     mounted(){
       
        API.getAllUsers()
         .then(details=>{
-            this.items[1]['members'] = details.data;
+            this.items[2]['members'] = details.data;
         })
         .catch(err=>{
             
         }) ;
       API.getAllMembers()
           .then((details)=>{
-              this.items[0]['members'] = details.data;
+              this.items[1]['members'] = details.data;
           })
           .catch((err)=>{
               
@@ -124,5 +176,35 @@ export default {
         
     }
 
-
+.coming-soon{
+  height: 250px;
+}
+h3 {
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  color: black;
+  font-family: Times New Roman, serif;
+  /* background: -webkit-linear-gradient(deeppink, yellow, deeppink, purple);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent; */
+  font-size: 6.5vw;
+  /* text-transform: uppercase; */
+}
+h3:before {
+  content: attr(data-text);
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform-origin: bottom;
+  transform: rotateX(180deg);
+  line-height: 1.14em;
+ background: linear-gradient(0deg, #000000 0, transparent 70%);
+ background-clip: text;
+ -webkit-text-color: transparent;
+  opacity: 0.3;
+}
 </style>
